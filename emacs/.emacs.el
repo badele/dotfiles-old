546cd3b907e4b27fa2aa6b595a310b660fc9b497
badele <bruno.adele@jesuislibre.org>
2013-01-13 21:39:24 +0100

    First version for python (pep8, flymake, autocomplete, rope)

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -1,37 +1,83 @@
-;;; A package manager
+;; A package manager
+
+
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+
 
 (setq my:el-get-packages
       '(virtualenv
-	pymacs
-	rope
-	ropemode
-	ropemacs
-	color-theme
+        magit
+        auto-complete
+        yasnippet
+        pymacs
+        rope
+        ropemode
+        ropemacs
+        flymake
+        flymake-cursor
+        python-pep8
+        nxhtml
+        color-theme
         color-theme-solarized
         ))				
- 
+
 (el-get 'sync my:el-get-packages)
+                                        ;(el-get 'sync)
+
+;; Key mapping
+(global-set-key [ (control c) (control g) ] 'goto-line);; goto line function C-c C-g
+
+;; Global options
+(exit-splash-screen) ;; No splash screen
+(global-linum-mode t);; Numerotation des lignes
+(cua-mode t) ;; Activation des racourcis couper/copier/coller
+(yas-global-mode t)  ;; Active le mode yasnippet
+(ido-mode t) ;; Ameliore la recherche de buffer et de fichier
+(color-theme-solarized-dark) ;; Solarizd color theme
+
+;; code completion
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Python (src: www.enigmacurry.com)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; Python
+
+;; Pymacs
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+
+;; Rope
 (pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
 
-;;; Gestionnaire de package
-;;;(require 'package)
-;;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-;;;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages") t)
-;;;(package-initialize)
+;;flymake
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pycheckers"  (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
 
-(color-theme-solarized-dark)
+
 
 ;;; Indentation
 (setq-default indent-tabs-mode nil)    ; use only spaces and no tabs
