c599a1614dbb2a4b9d51fbb9548a99b0a6427b65
badele <bruno.adele@jesuislibre.org>
2013-02-02 15:49:17 +0100

    Ajout d'un fichie de fonction

---
+++ user/bin/dotfiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+install_pkg ()
+{
+    PKG=""
+    INSTALL=0
+    for p in "$@";
+    do
+        pacman -Qi $p > /dev/null
+        if [ $? -eq 1 ]; then
+	        PKG="$PKG $p"
+            install=1
+        fi
+    done
+    echo "Installation de: $PKG"
+
+    if [ $INSTALL -eq 1 ]; then
+        yaourt -S $PKG
+    fi
+}
