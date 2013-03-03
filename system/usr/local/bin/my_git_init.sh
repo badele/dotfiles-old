4b9431d7cd5aade343a3d4a8566e2116541d48e7
badele <bruno.adele@jesuislibre.org>
2013-03-03 02:10:51 +0100

    Ajout script de création d'un dépot git

---
+++ system/usr/local/bin/my_git_init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DST=git@192.168.253.53
+PRJ=$1
+
+if [ "$PRJ" == "" ]; then
+	echo "veuillez saisir un nom de projet"
+	exit
+fi
+
+if [ ! -d ~/projets/$PRJ ]; then
+	# Creation du repertoire
+	mkdir ~/projets/$PRJ
+	cd ~/projets/$PRJ
+
+	# Création du repository
+	git init
+	cd ~/projets
+
+	# Clone et copie du repository vers le depot local
+	git clone --bare $PRJ $PRJ.git
+	scp -r ~/projets/$PRJ.git $DST:
+
+	# Suppression des projet temporaire
+	rm -r ~/projets/$PRJ
+	rm -r ~/projets/$PRJ.git
+
+	echo ""
+	echo "[Optionnel] git clone $DST:$PRJ.git"
+else
+	echo "le projet existe déja !"
+fi
+
+
+
+
