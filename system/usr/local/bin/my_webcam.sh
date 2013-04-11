59553911cf75b72177d26d78d2c8da7df37ec7fc
badele <bruno.adele@jesuislibre.org>
2013-04-11 22:16:58 +0200

    Ameliore les performances locale

--- system/usr/local/bin/my_webcam.sh
+++ system/usr/local/bin/my_webcam.sh
@@ -2,10 +2,17 @@
 
 # Tempo
 TKEY=100
-TCAM=3
+TCAM=4
+
+ip addr | grep 192.168.253  > /dev/null 2>&1
+if [ $? -eq 1 ]; then
+    IP="10.0.0.33"
+else
+    IP="192.168.253.9"
+fi
 
 webcam () {
-    vlc --no-audio http://10.0.0.33:$1&
+    vlc --no-audio http://$IP:$1&
     sleep $TCAM
     xdotool key --delay $TKEY ctrl+h 
 }
